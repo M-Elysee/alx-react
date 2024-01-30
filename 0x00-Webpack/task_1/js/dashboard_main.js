833e260e701cd99f578c5c82312fbefc312abe21
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2024-01-30 19:51:40 +0000

    0x00. Webpack|part2

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,11 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 let i = 0;
 
@@ -9,12 +14,4 @@ function updateCounter() {
     $('#count').text(`${i} clicks on the button`);
 }
 
-$(document).ready(function () {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  $('button').on('click', _.debounce(updateCounter, 500));
-});
+$('button').on('click', _.debounce(updateCounter, 500));
