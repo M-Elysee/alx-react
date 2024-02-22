6b6db472ffc541eab37bedb58b191d4c966d12e8
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2024-02-22 10:45:42 +0000

    0x06. React Immutable

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,5 +1,5 @@
-import 'immutable';
+import { is } from 'immutable';
 // a function that check if two immutable arrays are equal;
 export default function areMapsEqual (map1, map2) {
-  return map1.equals(map2);
+  return is(map1, map2);
 }
