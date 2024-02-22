e5ebbdcc889bffb7765cd160713b0ef4b3424137
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2024-02-22 10:42:29 +0000

    0x06. React Immutable

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,4 +1,6 @@
 import { Map } from 'immutable';
 
 export const map = Map({ 1: 'Liam', 2: 'Noah', 3: 'Elijah', 4: 'Oliver', 5: 'Jacob', 6: 'Lucas' });
-export const map2 = map.merge({ 2: 'Benjamin', 4: 'Oliver' });
+export const map2 = map.withMutations((value) => {
+  value.set(2, 'Benjamin').set(4, 'Oliver');
+});
--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,3 +1,4 @@
+import 'immutable';
 // a function that check if two immutable arrays are equal;
 export default function areMapsEqual (map1, map2) {
   return map1.equals(map2);
